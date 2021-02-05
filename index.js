01c19dd71c2d2fe86eae4e6da0391d3ff68aa00f
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-02-05 22:03:45 +0000

    added logic

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  return array.reduce(((acc, cur) => {
+    if (Array.isArray(cur)) {
+      return acc + sumItems(cur);
+    } else {
+      return acc + cur;
+    }
+  }), 0);
 }
 
 module.exports = sumItems;
